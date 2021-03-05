a123fcf9067ab41ece1db04c015003d529f8a399
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-03-05 13:50:45 +0000

    Reorganised the project and split+renamed few files

---
+++ scheme/keygen.go
@@ -0,0 +1,191 @@
+// Copyright 2021 Nym Technologies SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coconut
+
+import (
+	"github.com/consensys/gurvy/bls381"
+	"github.com/consensys/gurvy/bls381/fr"
+	"gitlab.nymte.ch/nym/coconut/coconutGo"
+	"gitlab.nymte.ch/nym/coconut/coconutGo/polynomial"
+	"gitlab.nymte.ch/nym/coconut/coconutGo/utils"
+	"math/big"
+)
+
+// SecretKey represents secret key of a Coconut signing authority.
+type SecretKey struct {
+	// TODO: big.Int or Fp.Element?
+	x  big.Int
+	ys []big.Int
+}
+
+func (sk *SecretKey) X() *big.Int {
+	return &sk.x
+}
+
+func (sk *SecretKey) Ys() *[]big.Int {
+	return &sk.ys
+}
+
+// Derive verification key using this secret key.
+func (sk *SecretKey) VerificationKey(params *coconutGo.Parameters) VerificationKey {
+	g2 := params.Gen2()
+
+	alpha := utils.G2ScalarMul(g2, &sk.x)
+	beta := make([]*bls381.G2Jac, len(sk.ys))
+	for i, y := range sk.ys {
+		betai := utils.G2ScalarMul(g2, &y)
+		beta[i] = &betai
+	}
+
+	return VerificationKey{
+		alpha: alpha,
+		beta:  beta,
+	}
+}
+
+// VerificationKey represents verification key of a Coconut signing authority.
+type VerificationKey struct {
+	alpha bls381.G2Jac
+	beta  []*bls381.G2Jac
+}
+
+// Alpha returns appropriate part of the the verification key
+func (vk *VerificationKey) Alpha() *bls381.G2Jac {
+	return &vk.alpha
+}
+
+// Beta returns appropriate part of the the verification key
+func (vk *VerificationKey) Beta() []*bls381.G2Jac {
+	return vk.beta
+}
+
+func (vk *VerificationKey) Equal(other *VerificationKey) bool {
+	if len(vk.beta) != len(other.beta) {
+		return false
+	}
+
+	if !utils.G2JacobianEqual(&vk.alpha, &other.alpha) {
+		return false
+	}
+
+	for i := 0; i < len(vk.beta); i++ {
+		if !utils.G2JacobianEqual(vk.beta[i], other.beta[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+type KeyPair struct {
+	secretKey       SecretKey
+	verificationKey VerificationKey
+
+	// Optional index value specifying polynomial point used during threshold key generation.
+	index *SignerIndex
+}
+
+// Generate a single Coconut keypair ((x, y0, y1...), (g2^x, g2^y0, ...)).
+// It is not suitable for threshold credentials as all subsequent calls to `keygen` generate keys
+// that are independent of each other.
+func Keygen(params *coconutGo.Parameters) (KeyPair, error) {
+	attributes := len(params.Hs())
+	x, err := params.RandomScalar()
+	if err != nil {
+		return KeyPair{}, err
+	}
+	ys := make([]big.Int, attributes)
+	for i := range ys {
+		ys[i], err = params.RandomScalar()
+		if err != nil {
+			return KeyPair{}, err
+		}
+	}
+
+	secretKey := SecretKey{
+		x:  x,
+		ys: ys,
+	}
+
+	return KeyPair{
+		secretKey:       secretKey,
+		verificationKey: secretKey.VerificationKey(params),
+	}, nil
+}
+
+// Generate a set of n Coconut keypairs [((x, y0, y1...), (g2^x, g2^y0, ...)), ...],
+// such that they support threshold aggregation by `threshold` number of parties.
+// It is expected that this procedure is executed by a Trusted Third Party.
+func TTPKeygen(params *coconutGo.Parameters, threshold uint64, numAuthorities uint64) ([]KeyPair, error) {
+	if threshold == 0 {
+		return nil, coconutGo.ErrZeroThreshold
+	}
+
+	if threshold > numAuthorities {
+		return nil, coconutGo.ErrInvalidThreshold
+	}
+
+	attributes := len(params.Hs())
+
+	// generate polynomials
+	v, err := polynomial.NewRandomPolynomial(params, int(threshold-1))
+	if err != nil {
+		return nil, err
+	}
+
+	ws := make([]polynomial.Polynomial, attributes)
+	for i := 0; i < attributes; i++ {
+		w, err := polynomial.NewRandomPolynomial(params, int(threshold-1))
+		if err != nil {
+			return nil, err
+		}
+		ws[i] = w
+	}
+
+	// TODO: potentially if we had some known authority identifier we could use that instead
+	// of the increasing (1,2,3,...) sequence
+	//polynomialIndices := make([]uint64, numAuthorities)
+
+	secretKeys := make([]SecretKey, numAuthorities)
+
+	// generate polynomial shares
+	for i := 0; i < int(numAuthorities); i++ {
+		index := big.NewInt(int64(i + 1))
+
+		x := v.Evaluate(index, fr.Modulus())
+
+		ys := make([]big.Int, attributes)
+		for j := 0; j < len(ws); j++ {
+			ys[j] = ws[j].Evaluate(index, fr.Modulus())
+		}
+
+		secretKeys[i] = SecretKey{
+			x:  x,
+			ys: ys,
+		}
+	}
+
+	keypairs := make([]KeyPair, numAuthorities)
+	for i := 0; i < int(numAuthorities); i++ {
+		verificationKey := secretKeys[i].VerificationKey(params)
+		index := uint64(i + 1)
+		keypairs[i] = KeyPair{
+			secretKey:       secretKeys[i],
+			verificationKey: verificationKey,
+			index:           &index,
+		}
+	}
+
+	return keypairs, nil
+}
---
+++ utils/helpers.go
@@ -0,0 +1,217 @@
+// Copyright 2021 Nym Technologies SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"crypto"
+	"encoding/binary"
+	"errors"
+	"github.com/consensys/gurvy/bls381"
+	"github.com/consensys/gurvy/bls381/fp"
+	"github.com/consensys/gurvy/bls381/fr"
+	_ "golang.org/x/crypto/sha3"
+	"io"
+	"math/big"
+)
+
+// R^2 = 2^512 mod q
+var R2 = fr.Element{
+	14526898881837571181,
+	3129137299524312099,
+	419701826671360399,
+	524908885293268753,
+}
+
+// R^3 = 2^768 mod q
+var R3 = fr.Element{
+	14279814937963099055,
+	1963020886675057040,
+	8345518043873801240,
+	7938258146690806761,
+}
+
+// B = 4, i.e. the curve coefficient
+var B = fp.Element{
+	12260768510540316659,
+	6038201419376623626,
+	5156596810353639551,
+	12813724723179037911,
+	10288881524157229871,
+	708830206584151678,
+}
+
+func HashToG1(msg []byte) bls381.G1Affine {
+	return incrementAndCheck(msg)
+}
+
+// Attempts to deserialize an uncompressed element, not checking if the
+// element is in the correct subgroup.
+// basically copied code from `func (p *G1Affine) SetBytes(buf []byte) (int, error)`
+// but without the subgroup check
+func g1AffineFromBytesUnchecked(bytes []byte) (bls381.G1Affine, error) {
+	if len(bytes) < bls381.SizeOfG1AffineCompressed {
+		return bls381.G1Affine{}, io.ErrShortBuffer
+	}
+
+	// recover flags
+	compressionFlagSet := ((bytes[0] >> 7) & 1) == 1
+	infinityFlagSet := ((bytes[0] >> 6) & 1) == 1
+	largest := ((bytes[0] >> 5) & 1) == 1
+
+	// first bit must be set - otherwise it implies uncompressed form (i.e. 96 bytes)
+	// second bit must not be set - otherwise it implies the point at infinity
+	if !compressionFlagSet || infinityFlagSet {
+		return bls381.G1Affine{}, io.ErrShortBuffer
+	}
+
+	var p bls381.G1Affine
+
+	originalVal := bytes[0]
+	// mask away the flags
+	bytes[0] &= 0b0001_1111
+
+	p.X.SetBytes(bytes[0 : 0+fp.Bytes])
+
+	// solve the curve equation to compute Y (y = sqrt(x^3 + 4))
+	var YSquared, Y fp.Element
+
+	YSquared.Square(&p.X).Mul(&YSquared, &p.X)
+	YSquared.Add(&YSquared, &B)
+	if Y.Sqrt(&YSquared) == nil {
+		return bls381.G1Affine{}, errors.New("invalid compressed coordinate: square root doesn't exist")
+	}
+
+	if Y.LexicographicallyLargest() {
+		// Y ">" -Y
+		if !largest {
+			Y.Neg(&Y)
+		}
+	} else {
+		// Y "<=" -Y
+		if largest {
+			Y.Neg(&Y)
+		}
+	}
+
+	p.Y = Y
+
+	// restore flags
+	bytes[0] = originalVal
+
+	return p, nil
+}
+
+func incrementAndCheck(msg []byte) bls381.G1Affine {
+	// reason for sha3 384 is for the 48 bytes output and it's a good enough solution
+	// for the temporary use it has
+	h := crypto.SHA3_384.New()
+
+	ctr := uint64(0)
+	for {
+		ctrBytes := make([]byte, 8)
+		binary.LittleEndian.PutUint64(ctrBytes, ctr)
+
+		h.Write(msg)
+		h.Write(ctrBytes)
+		digest := h.Sum([]byte{})
+		h.Reset()
+
+		ctr += 1
+
+		p, err := g1AffineFromBytesUnchecked(digest)
+		if err != nil {
+			continue
+		} else {
+			p.ClearCofactor(&p)
+			return p
+		}
+	}
+}
+
+func ReverseBytes(bytes []byte) []byte {
+	bytesNew := make([]byte, len(bytes))
+	for i := 0; i < len(bytes); i++ {
+		bytesNew[i] = bytes[len(bytes)-i-1]
+	}
+	return bytesNew
+}
+
+// do it the same way zcash is doing it in the rust library
+func ScalarFromBytesWide(bytes [64]byte) big.Int {
+	var d0 fr.Element
+	var d1 fr.Element
+	// recover limbs
+
+	d0[0] = binary.LittleEndian.Uint64(bytes[0:8])
+	d0[1] = binary.LittleEndian.Uint64(bytes[8:16])
+	d0[2] = binary.LittleEndian.Uint64(bytes[16:24])
+	d0[3] = binary.LittleEndian.Uint64(bytes[24:32])
+
+	d1[0] = binary.LittleEndian.Uint64(bytes[32:40])
+	d1[1] = binary.LittleEndian.Uint64(bytes[40:48])
+	d1[2] = binary.LittleEndian.Uint64(bytes[48:56])
+	d1[3] = binary.LittleEndian.Uint64(bytes[56:64])
+
+	// Convert to Montgomery form
+	// d0 * R2 + d1 * R3
+	var t1 fr.Element
+	t1.Mul(&d0, &R2)
+
+	var t2 fr.Element
+	t2.Mul(&d1, &R3)
+
+	var res fr.Element
+	res.Add(&t1, &t2)
+
+	var resBI big.Int
+	res.ToBigIntRegular(&resBI)
+
+	return resBI
+}
+
+func ScalarToLittleEndian(scalar *big.Int) [32]byte {
+	var frScalar fr.Element
+	// ensure correct order and width
+	frScalar.SetBigInt(scalar)
+	scalarBytes := frScalar.Bytes()
+
+	var out [32]byte
+	for i := 0; i < 32; i++ {
+		out[31-i] = scalarBytes[i]
+	}
+
+	return out
+}
+
+func ScalarFromLittleEndian(bytes []byte) big.Int {
+	var s big.Int
+	// TODO: or do I need to reverse per octet?
+
+	s.SetBytes(ReverseBytes(bytes))
+	return s
+}
+
+func G1JacobianFromBytes(bytes []byte) (bls381.G1Jac, error) {
+	var pAff bls381.G1Affine
+	_, err := pAff.SetBytes(bytes)
+	if err != nil {
+		return bls381.G1Jac{}, err
+	}
+
+	var p bls381.G1Jac
+	p.FromAffine(&pAff)
+
+	return p, nil
+}
